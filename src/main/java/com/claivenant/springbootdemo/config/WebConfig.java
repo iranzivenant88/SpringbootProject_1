1a22f4c9b4d52af376fbddb0c806d70c64f6431e
iranzivenant88 <88venant@gmail.com>
2023-03-24 19:20:13 -0600

    file5

---
+++ src/main/java/com/claivenant/springbootdemo/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.claivenant.springbootdemo.config;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorParameter(true)
+                .parameterName("mediaType")
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("xml",MediaType.APPLICATION_XML)
+                .mediaType("json",MediaType.APPLICATION_JSON);
+    }
+}
