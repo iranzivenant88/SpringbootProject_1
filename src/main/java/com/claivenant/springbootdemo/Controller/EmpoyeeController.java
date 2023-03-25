8a58a7f1175227ad06eacba7a5a5633bc7e93074
iranzivenant88 <88venant@gmail.com>
2023-03-24 19:15:21 -0600

    file-11

--- src/main/java/com/claivenant/springbootdemo/Controller/EmpoyeeController.java
+++ src/main/java/com/claivenant/springbootdemo/Controller/EmpoyeeController.java
@@ -3,16 +3,20 @@ package com.claivenant.springbootdemo.Controller;
 import com.claivenant.springbootdemo.Model.Employee;
 import com.claivenant.springbootdemo.Service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 
-@RequestMapping("/employee")
+@RequestMapping("/v1/employees")
 public class EmpoyeeController {
+    @Qualifier("employeeServiceImpl")
+
     @Autowired
     private EmployeeService employeeService;
+
     @PostMapping
     public Employee save(@RequestBody Employee employee){
         return employeeService.save(employee);
@@ -27,5 +31,9 @@ public class EmpoyeeController {
         return employeeService.getEmployeeById(id);
 
 
+    }
+    @DeleteMapping("/{id}")
+    public String deleteEmployeeById(@PathVariable String id){
+        return employeeService.deleteEmployeeById(id);
     }
 }
