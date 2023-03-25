cf646bd0b0d11d3c1b1fdb2fb53ee7cf72275627
iranzivenant88 <88venant@gmail.com>
2023-03-24 19:13:54 -0600

    file8

---
+++ src/main/java/com/claivenant/springbootdemo/Repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.claivenant.springbootdemo.Repository;
+
+import com.claivenant.springbootdemo.Entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity,String> {
+}
